46e6842fd500a92ace900a87c6f4eb98d866c0a6
Ekundayo Olaoluwa Stephen <ekundayoolaoluwa69@gmail.com>
2026-06-17 16:14:03 -0400

    package

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  swcMinify: true,
-  turbopack: {},
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-    return config;
-  },
 };
 
 module.exports = nextConfig;
